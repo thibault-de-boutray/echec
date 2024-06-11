b57383c51c16ca5dc7e316f1435feaf90cb6192b
thibault-de-boutray <thibault.de-boutray@etu.utc.fr>
2024-06-11 17:03:33 +0200

    ok

---
+++ menu.php
@@ -0,0 +1,24 @@
+
+<header>
+    <ul class="menu">
+        <li class="section-menu">
+            <a href="#" class="element-menu">premier</a>
+            <ul class="section-sous-menu">
+                <li class="element-sous-menu">element 2</li>
+                <li class="element-sous-menu">element 3</li>
+                <li class="element-sous-menu">element 1</li>
+            </ul>
+        </li>
+        <li class="section-menu">
+            <a href="#" class="element-menu">deuxième</a>
+            <ul class="section-sous-menu">
+                <li class="element-sous-menu">element 1</li>
+                <li class="element-sous-menu">element 2</li>
+                  <li class="element-sous-menu">element 3</li>
+            </ul>
+        </li>
+    </ul>
+    <div>
+
+    </div>
+</header>
